fd72bfc2b909b181d8876d5097c81456c2135c90
Thierry Bwuzure <bwuzure@gmail.com>
2026-04-01 20:17:41 +0200

    Add index.php + catalog & product file

--- index.php
+++ index.php
@@ -8,7 +8,15 @@
 </head>
 
 <body>
-
+    <article>
+        <img src="" alt="">
+        <img src="" alt="">
+        <h3></h3>
+        <strong></strong>
+        <strong></strong>
+        <li></li>
+        <a href=""></a>
+    </article>
 </body>
 
 </html>
