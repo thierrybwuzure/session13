3e93aa91d12767ad17a0fca91a9e6bdce4ab6b4c
Thierry Bwuzure <bwuzure@gmail.com>
2026-04-01 20:29:20 +0200

    Modify file

---
+++ catalog.php
@@ -0,0 +1,9 @@
+<article>
+    <img src="" alt="">
+    <img src="" alt="">
+    <h3></h3>
+    <strong></strong>
+    <strong></strong>
+    <li></li>
+    <a href=""></a>
+</article>
---
+++ detail.php
@@ -0,0 +1,15 @@
+<article>
+    <img src="" alt="">
+    <img src="" alt="">
+    <h3>Detail</h3>
+    <strong>Type</strong>
+    <strong>€</strong>
+    <ol>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+        <li>*</li>
+    </ol>
+    <a href=""></a>
+</article>
